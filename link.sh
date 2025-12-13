6c19d6ff759384cd2c4b0db11325baa65f06c7f6
Richard <richard760x@hotmail.com>
2025-12-12 23:31:04 -0500

    posimacs theme

--- link.sh
+++ link.sh
@@ -5,4 +5,5 @@ set -x
 mkdir -p ~/.emacs.d/packages
 find packages -name "*.el" | xargs realpath | xargs -I{} ln -sf {} ~/.emacs.d/packages/.
 ln -sf $(realpath .emacs) ~/.emacs
-ls -l ~/.emacs ~/.emacs.d/packages | grep "\->"
+ln -sf $(realpath themes) ~/.emacs.d/themes
+ls -l ~/.emacs ~/.emacs.d/packages ~/.emacs.d/themes | grep "\->"
